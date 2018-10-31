8b2cab094f2e879b67d5be89762aabffe9d2d0d1
Leediyal <lydia14jeba@gmail.com>
2018-10-31 18:42:50 +0530

    Adding image.alt Property

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  //image.alt = restaurant.name + " restaurant photograph";
+  image.alt = restaurant.name + " restaurant photograph";
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
