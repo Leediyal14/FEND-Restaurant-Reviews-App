e56201f752ac5d9a43e43e0985b4c0b39605b402
Leediyal <lydia14jeba@gmail.com>
2018-11-01 17:11:36 +0530

    Fix: Service Worker

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 console.log('Service Worker Registered Successfully!');
 
 // Storing files in the Cache
-var myCache = 'restaurant-cache';
-var cachedFiles = [
+var CACHE_NAME = 'restaurant-cache';
+var urlsToCache = [
     '/',
     '/index.html',
     '/restaurant.html',
@@ -26,45 +26,12 @@ var cachedFiles = [
 
 //Installing the Service Worker
 self.addEventListener('install', function(event) {
+    // Perform install steps
     event.waitUntil(
-        caches.open(myCache).then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(cachedFiles);
-        })
-    );
-});
-
-// Fetching Event
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-
-                // Clone the request
-                var fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest).then(
-                    function (response) {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // Clone the response
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(function (cache) {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
             })
     );
 });
@@ -84,5 +51,40 @@ self.addEventListener('activate', function (event) {
                 })
             );
         })
+    );
+});
+
+// Fetching Event
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                // Clone the request
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
     );
 });
