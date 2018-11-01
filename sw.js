2982e553b766978f011ee1c2185249d63fcf476e
Leediyal <lydia14jeba@gmail.com>
2018-11-01 18:08:20 +0530

    Fix: Updating SW

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 console.log('Service Worker Registered Successfully!');
 
 // Storing files in the Cache
-var CACHE_NAME = 'restaurant-cache';
+var CACHE_NAME = 'my-restaurant-cache-v1';
 var urlsToCache = [
     '/',
     '/index.html',
@@ -25,13 +25,48 @@ var urlsToCache = [
 ];
 
 //Installing the Service Worker
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
+            .then(function (cache) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetching Event
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                // Clone the request
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
             })
     );
 });
@@ -51,40 +86,5 @@ self.addEventListener('activate', function (event) {
                 })
             );
         })
-    );
-});
-
-// Fetching Event
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-
-                // Clone the request
-                var fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest).then(
-                    function(response) {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // Clone the response
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(function(cache) {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
-            })
     );
 });
